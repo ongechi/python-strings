e5926b87a468e1245989ef6c3c5fc899989076ca
ongechi <77966899+ongechi@users.noreply.github.com>
2021-03-08 08:59:22 +0300

    Update strings.py

--- strings.py
+++ strings.py
@@ -1,4 +1,4 @@
-#python strings#
+#python strings- Python does not have a character data type, a single character is simply a string with a length of 1
 a="Patrick Matayo Ongechi"
 b='welcome to python strings'
 c='''strings'''
